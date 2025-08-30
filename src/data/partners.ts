0e3c1d0a14350472cfdd3bb23e9b11f90d48fae2
rockske8er <rockske8er@gmail.com>
2025-08-30 12:00:00 -0300

    feat:partners

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,62 +1,35 @@
 export const partners = [
   {
     id: 1,
-    name: 'Alex Net',
-    image: '/partners/alex.png',
+    name: "Alex Net",
+    image: "/partners/alex.png",
   },
-  {
-    id: 2,
-    name: 'Bruno Vanderlei',
-    image: '/partners/bruno.png',
-  },
+
   {
     id: 3,
-    name: 'HG Vidraçaria',
-    image: '/partners/hg.png',
+    name: "HG Vidraçaria",
+    image: "/partners/hg.png",
   },
   {
     id: 4,
-    name: 'Dr. Islan Maik',
-    image: '/partners/islan.png',
+    name: "Dr. Islan Maik",
+    image: "/partners/islan.png",
   },
-  {
-    id: 5,
-    name: 'MA Celular ',
-    image: '/partners/mia.png',
-  },
-  {
-    id: 6,
-    name: 'Panificadora Martins',
-    image: '/partners/panificadora.png',
-  },
-  {
-    id: 7,
-    name: 'Segel',
-    image: '/partners/segel.png',
-  },
-  {
-    id: 8,
-    name: 'Supermercado Tio correia',
-    image: '/partners/tio-correia.png',
-  },
+
   {
     id: 9,
-    name: 'Edivaldo Lanches',
-    image: '/partners/edivaldo.png',
+    name: "Edivaldo Lanches",
+    image: "/partners/edivaldo.png",
   },
   {
     id: 10,
-    name: 'Prefeitura de Serra do Ramalho',
-    image: '/partners/prefeitura.png',
+    name: "Prefeitura de Serra do Ramalho",
+    image: "/partners/prefeitura.png",
   },
-  {
-    id: 11,
-    name: 'Serrado',
-    image: '/partners/serrado.png',
-  },
+
   {
     id: 12,
-    name: 'Véi da 11',
-    image: '/partners/vei.png',
+    name: "Véi da 11",
+    image: "/partners/vei.png",
   },
-]
+];
